0e4fcae95337f15cbea7b491a7480a6445cf9fbf
Xiaoshan-jun <90107845+Xiaoshan-jun@users.noreply.github.com>
2023-11-07 16:31:07 -0800

    C1

--- datasetBuilder.py
+++ datasetBuilder.py
@@ -14,20 +14,20 @@ import numpy as np
 from tqdm import tqdm
 np.random.seed(80) 
 safec = 0.9
-dim = 64
+dim = 32
 num_elements = dim * dim
 for i in tqdm(range(0, 500)):
     #create random risky map
-    safeplace = int(0.5 * num_elements)
+    safeplace = int(0.7 * num_elements)
     lowrisk = int(0 * num_elements)
     highrisk = num_elements - lowrisk- safeplace
     values_safeplace = np.random.uniform(0, 0, safeplace)
     values_lowrisk = np.random.uniform(0, 0.05, lowrisk)
     values_highrisk = np.random.uniform(1, 1, highrisk)
     combined_values = np.concatenate((values_safeplace, values_lowrisk, values_highrisk))
+    np.random.shuffle(combined_values)     # Shuffle the combined values
     UAVmap = combined_values.reshape(dim, dim)
     print(UAVmap)
-    np.random.shuffle(combined_values)     # Shuffle the combined values
     mapname = '64absoluteMap/' + str(dim) + '_' + str(i) + '.npy'
     np.save(mapname, UAVmap)
     grids = []
@@ -35,6 +35,9 @@ for i in tqdm(range(0, 500)):
         for y in range(dim):
             grids.append((x, y))
     for xG in tqdm(grids):
+        if UAVmap[xG[0]][xG[1]] == 1:
+            #print('in')
+            continue
         Hvalue = np.zeros((dim, dim))
         for xI in grids:
             xI = (xI[0], xI[1], 1)
